1cbb8b7c85c6df6f7243f430f0bd53623f318c80
mrm <matt.rmoore@gmail.com>
2021-01-23 22:49:43 -0600

    Connection to local API now working

--- src/app/sharp/sharp.component.ts
+++ src/app/sharp/sharp.component.ts
@@ -11,11 +11,19 @@ export class SharpComponent implements OnInit {
 
   constructor(private apiService: ApiService) { }
 
+  // made prescribed changes to Startup.cs as mentioned in:
+  // https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-5.0#set-the-allowed-origins
+  // and 
+  // https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-5.0
+
+  // also authorized the possible security risk in the browser to allow CORS
+
   ngOnInit(): void {
-    // this.apiService.getSharp().subscribe((data)=>{
-      // console.log(data);
-      // this.sharp = data['sharp']
-    // });
+    this.apiService.getSharp().subscribe((data)=>{
+      console.log("C-Sharp API");
+      console.log(data);
+      this.sharp = data
+    });
 
       }
 
